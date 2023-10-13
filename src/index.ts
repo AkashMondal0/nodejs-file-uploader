628e09a22fb054c4517b4af64649509c187bea7f
Akash Mondal <akash2003mondal@gmail.com>
2023-10-13 19:57:12 +0530

    fixed - and deploy setup with github action

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import UserRouter from './routes/User';
-import UploadRouter from './routes/Upload';
-import StorageRouter from './routes/Storage';
+import FileRouter from './routes/File';
 
 dotenv.config();
-const host = process.env.HOST || 'localhost';
-const port = process.env.PORT || 3000;   
+const host = process.env.PRODUCTION_HOST || 'http://localhost:4000';
+const port = process.env.PORT || 4000;   
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -14,14 +13,13 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use('/user', UserRouter)
-app.use('/upload', UploadRouter)
-app.use('/storage', StorageRouter);
+app.use('/file', FileRouter)
 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(3000, () => {
-    console.log(`Server running at ${host}:${port}/`);
+app.listen(port, () => {
+    console.log(`Server running at ${host}`);
 });
